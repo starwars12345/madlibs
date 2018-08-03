bd142cc5b3f7706b3ede225a2220bbc46d37f9d5
starwars12345 <emmableau@hotmail.com>
2018-08-03 14:41:14 -0600

    Add files via upload

---
+++ final project.py
@@ -0,0 +1,70 @@
+animal = input("ENTER AN ANIMAL")
+
+name = input("ENTER THE ANIMALS NAME (YOU ARE THIS ANIMAL)")
+print("Once there was a(n) " + animal + " who was named " + name + ".")
+
+pronoun = input("ENTER A PRONOUN(IT IS THE ANIMALS)")
+title = input("ENTER YOUR NAME")
+print(pronoun + " was an orphan, who was adopted by " + title + ".")
+
+print(name + " Doesn't like this person, so " + pronoun +" hatched a plan.")
+
+noun = input("ENTER A NOUN")
+print(pronoun + " is going to make a " + noun + ", with this " + noun + " , " + pronoun + " will go back in time.")
+
+verb = input("ENTER A VERB(ING) ")
+print("While " + name + " was " + verb + " , " + pronoun + " made a list of materials " + pronoun + " will need.")
+
+item_1 = input("ENTER A NOUN")
+item_2 = input("ENTER A NOUN")
+item_3 = input("ENTER A NOUN")
+item_4 = input("ENTER A NOUN")
+item_5 = input("ENTER A NOUN")
+adjective = input("ENTER AN ADJECTIVE")
+print("The list consists of, a(n) " + adjective + " "  + item_1 + ", " + item_2 + ", " + item_3 + ", " + item_4 + ", and a(n) " + item_5 + " .")
+
+verb_2 = input("ENTER A VERB(NO ING)")
+verb_3 = input("ENTER A VERB(NO ING)")
+verb_4 = input("ENTER A VERB(NO ING)")
+adverb = input("ENTER AN ADVERB ")
+item_6 = input("ENTER A NOUN")
+adjective = input("ENTER AN ADJECTIVE ")
+print("To get " + adjective + " " + item_1 + " " + pronoun + " had to " + verb_2 + " up the mountain and " + adverb + " " + verb_3 + " a " + item_6 + " and " + verb_4 + " it back home.")
+
+verb_5 = input("ENTER A VERB(NO ING)")
+verb_2 = input("ENTER AN ADVERB ")
+item_7 = input("ENTER A NOUN")
+place = input("NAME A PLACE")
+conjunction = input("ENTER A CONJUNCTION")
+god = input("ENTER THE NAME OF A GOD")
+print("To retrive " + item_2 + " " + pronoun + " must " + adverb + " " + verb_5 + " a " + item_7 + " to " + place + " and back " + conjunction + " must also get it blessed by " + god + ".")
+
+animal_2 = input("ENTER AN ANINMAL")
+adjective_2 = input("ENTER AND ADJECTIVE")
+phrase = input("ENTER A PHRASE (INCLUDE SPACES AND PUNCUATION)")
+print("To obtain " + adjective_2 + " " + animal_2 + " will give you the/a " + item_3 + " to take on your journey.")
+
+verb_6 = input("ENTER A VERB(NO ING)")
+race = input("ENTER A TYPE OF RACE (TRIATHILON, BIATHOLON, MARATHON, ETC)")
+compeltion = input("ENTER WIN OR LOSE")
+print("To earn a " + item_4 + ", " + " you must " + verb_6 + " a " + race + ".")
+
+print("You must " + compeltion + " to receive a " + item_4 + ".")
+
+verb_7 = input("ENTER A VERB(NO ING)")
+item_8 = input("ENTER A NOUN")
+print(" The last item will be the hardest to gain. You must " + verb_7 + " through a maze of " + item_8 + ".")
+
+container = input("ENTER A TYPE OF CONTAINER (BACKPACK, BOX, ETC.)")
+print(item_5 + " will come with a " + container + ", you must place all of your items into the " + container + ".")
+
+room = input("ENTER A TYPE OF ROOM (BATHROOM, SCIENCE LAS, ETC)")
+print("When all of the items are found go back to your house and make a " + room + ", once it is built construct the " + noun + ".")
+
+print("Now that the " + room + " and the " + noun + " are built, you can go back in time.")
+
+tense = input("ENTER HIS OR HER")
+past = input("ENTER ADOPTED OR FOUND")
+print("Why is " + pronoun + " going back in time, you might ask, " + pronoun + " is going back in time so that " + pronoun + " can get away from " + tense + " person." + pronoun + " wants this so " + pronoun + " can never be " + past + " by the same person again.")
+
+print("THE END")
